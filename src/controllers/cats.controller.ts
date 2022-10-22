8f78cc3f7ec846ce9477cd1a398dd995c0c1aac0
karthik2265 <63234167+karthik2265@users.noreply.github.com>
2022-10-22 23:01:11 +0530

    overview: controllers

--- src/controllers/cats.controller.ts
+++ src/controllers/cats.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Param, Req, Res } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Req, Res } from "@nestjs/common";
+import { request } from "http";
+import { CreateCatDto } from "src/Dto/create-cat-dto";
 
 @Controller("cats")
 export class CatsController {
@@ -11,5 +13,11 @@ export class CatsController {
   @Get(":id")
   findOne(@Param("id") id): string {
     return `found a cat with id = ${id}`;
+  }
+
+  @Post()
+  createOne(@Body() catDto: CreateCatDto, @Req() request): CreateCatDto {
+    console.table(catDto);
+    return catDto;
   }
 }
